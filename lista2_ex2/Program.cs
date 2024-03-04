3cfa0f5ef0b21cdc7185cf1ac62d329b55def671
ALUNO <ALUNO@LABFPC26>
2024-03-04 08:55:31 -0300

    Adicionar arquivos de projeto.

---
+++ lista2_ex2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista2_ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            for (int i = x+1; i < x+7; i++ )
+            
+                Console.WriteLine(i+2);
+            }
+        }
+    }
+}
